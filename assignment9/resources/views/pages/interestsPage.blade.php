761846043b880f670ad97a1c33986a15cc2c36ea
Promit Bhattacharjee <promitbhattacharjee.work@gmail.com>
2023-11-26 22:48:51 +0600

    first commit

---
+++ assignment9/resources/views/pages/interestsPage.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@include('components.navbar')
+@section('content')
+<!-- Interests-->
+<section class="resume-section" id="interests">
+    <div class="resume-section-content">
+        <h2 class="mb-5 text-white">Interests</h2>
+        <p class="text-align-justify text-white"> Complicated technologies, such as robotics and AI, excite me the most. In my free time, I enjoy going for long drives on my bike. Additionally, I have a penchant for watching thriller movies. Playing outdoor games helps me unwind and remain stress-free. Moreover, I absolutely love listening to songs whenever I have the opportunity.</p>
+    </div>
+</section>
+<hr class="text-primary">
+ @endsection
